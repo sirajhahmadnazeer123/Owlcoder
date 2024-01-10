4e7ad5254c0c539e500e8b49e1d7bc9c6dc08b0c
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2024-01-10 23:50:02 +0530

    Add files via upload

---
+++ Count possible ways to construct buildings.py
@@ -0,0 +1,10 @@
+class Solution:
+	def TotalWays(self, N):
+		b= s = 1
+		res = 0
+		mod = 1000000007
+		for i in range(N):
+		    res = (b%mod+s%mod)%mod
+		    b = s%mod
+		    s = res%mod
+	    return (res*res)%mod
